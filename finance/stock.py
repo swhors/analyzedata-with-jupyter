3ba8daa81964fba260a949572b0389a563e2f9a5
Won Seo <swhors@naver.com>
2024-09-09 19:03:21 +0900

    [dev 01] add job, fix mysql

--- finance/stock.py
+++ finance/stock.py
@@ -1,3 +1,5 @@
+class_name="Stock"
+
 class Stock:
     stype: str
     code: str
@@ -30,20 +32,21 @@ class Stock:
         self.memo = memo
 
     def __str__(self) -> str:
-        return f"{self.stype}, {self.code}, {self.name}, {self.market}, {self.sector}, {self.industry}, {self.listing_date}, {self.settle_month}, {self.memo}"
+        return f'\"{self.stype}\", \"{self.code}\", \"{self.name}\", \"{self.market}\", \"{self.sector}\", \"{self.industry}\", \"{self.listing_date}\", \"{self.settle_month}\", \"{self.memo}\"'
 
-def dict_2_stock(dict_stock):
-    """
-    'Code': '095570', 'Name': 'AJ네트웍스', 'Market': 'KOSPI', 'Sector': '산업용 기계 및 장비 임대업', 'Industry': '렌탈(파렛트, OA장비, 건설장비)', 'ListingDate': Timestamp('2015-08-21 00:00:00'), 'SettleMonth': '12월', 'Representative': '손삼달', 'HomePage': 'http://www.ajnet.co.kr', 'Region': '서울특별시'
-    """
-    return Stock(
-        dict_stock["Market"],
-        dict_stock["Code"],
-        dict_stock["Name"],
-        dict_stock["Market"],
-        dict_stock["Sector"],
-        dict_stock["Industry"],
-        dict_stock["ListingDate"],
-        dict_stock["SettleMonth"],
-        f'Representative={dict_stock["Representative"]}, HomePage={dict_stock["HomePage"]}, Region={dict_stock["Region"]}'
+    @classmethod
+    def dict_2_stock(cls, dict_stock):
+        """
+        'Code': '095570', 'Name': 'AJ네트웍스', 'Market': 'KOSPI', 'Sector': '산업용 기계 및 장비 임대업', 'Industry': '렌탈(파렛트, OA장비, 건설장비)', 'ListingDate': Timestamp('2015-08-21 00:00:00'), 'SettleMonth': '12월', 'Representative': '손삼달', 'HomePage': 'http://www.ajnet.co.kr', 'Region': '서울특별시'
+        """
+        return Stock(
+            dict_stock["Market"],
+            dict_stock["Code"],
+            dict_stock["Name"],
+            dict_stock["Market"],
+            dict_stock["Sector"],
+            dict_stock["Industry"],
+            dict_stock["ListingDate"],
+            dict_stock["SettleMonth"],
+            f'Representative={dict_stock["Representative"]}, HomePage={dict_stock["HomePage"]}, Region={dict_stock["Region"]}'
     )
